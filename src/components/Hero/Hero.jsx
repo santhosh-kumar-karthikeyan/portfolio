6baa47a3adbb649b662d01d61dc1384395ac6ff4
Santhosh Kumar Karthikeyan <dhanamsanthosh2005@gmail.com>
2025-06-22 21:33:23 +0530

    add hero section with image and intro

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,12 @@ import styles from "./Hero.module.css";
 export default function Hero() {
     return (
         <main className={styles.hero}>
-            <img src="" alt="main image" />
-            <h1>Hi, I am Santhosh Kumar Karthikeyan. </h1>
+            <img className={styles.mainImage} src="/assets/me.png" alt="main image" />
+            <section>
+            <h2>Hi, I am Santhosh Kumar Karthikeyan. </h2>
+            <h2>This is a portal to check out the works that I do, the tools the I use, the skills that I have acquired.</h2>
+            <h4 className={styles.mutedText}>Btw, ignore the "candid" picture of mine exisiting above, couldn't find a good picture of mine</h4>
+            </section>
         </main>
     );
 }
